ee7efb1ed5b649306c4270bc7e67021a5f901ac0
Alexandre Jurquet <a.jurquet@gmail.com>
2021-06-07 10:48:56 +0200

    commit

--- scripts.js
+++ scripts.js
@@ -10,8 +10,16 @@ fetch(mainEntryUrl + "?sort_by=-imdb_score")
     })
 
     .then(function(value) {
-        let best_movie_el = document.getElementById("best_movie_alltime")
-        best_movie_el.innerHTML = value.results[0].title;
+        let best_movie_el = document.getElementById("best_movie")
+        best_movie_el.innerText = value.results[0].title
+        
+        // best_movie_el.innerHTML = <ul>
+        //     <li>value.results[0].title</li>
+        //     <li>value.results[0].title</li>
+        //     <li>value.results[0].title</li>
+        //     <li>value.results[0].title</li>
+        //     <li>value.results[0].title</li>
+        //     </ul>;
         // console.log(value.results[0].title);
     })
 
