df58aa316448677251f17b8000793ee3594d3129
Alexandre Jurquet <a.jurquet@gmail.com>
2021-06-08 16:37:20 +0200

    essai fenêtre modale

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
-const fetch = require("node-fetch");
+// const fetch = require("node-fetch");
 const mainEntryUrl = "http://localhost:8000/api/v1/titles/";
+let movie_image_url = "";
+let movie_datas = [];
 
 
 fetch(mainEntryUrl + "?sort_by=-imdb_score")
@@ -9,20 +11,26 @@ fetch(mainEntryUrl + "?sort_by=-imdb_score")
         }
     })
 
-    .then(function(value) {
-        let best_movie_el = document.getElementById("best_movie")
-        best_movie_el.innerText = value.results[0].title
-        
-        // best_movie_el.innerHTML = <ul>
-        //     <li>value.results[0].title</li>
-        //     <li>value.results[0].title</li>
-        //     <li>value.results[0].title</li>
-        //     <li>value.results[0].title</li>
-        //     <li>value.results[0].title</li>
-        //     </ul>;
-        // console.log(value.results[0].title);
+    .then(function(data) {
+        movie_image_url = data.results[0].image_url;
+        movie_datas.push(data.results[0].title, data.results[0].genres,data.results[0].year,
+             data.results[0].imdb_score, data.results[0].directors, data.results[0].actors);
+
+        let best_movie_image_element = document.getElementById("bestMovie__image");
+        best_movie_image_element.innerHTML = "<p><img src=" +  movie_image_url + "</p>";
+
+        let best_movie_datas_element = document.getElementById("bestMovie__datas");
+        best_movie_datas_element.innerHTML = "<p><strong>" + movie_datas[0] + "</strong>" 
+
+        let best_movie_btn_element = document.getElementById("bestMovie__datas__btn");
+        best_movie_btn_element.innerHTML = "<a href='modal-btn'>Infos</a>"
+    
+                                
+        // console.log(movie_datas, movie_image_url)
+
     })
 
-    .catch(function(err) {
-        // Une erreur est survenue
+    .catch(function(error) {
+        console.log(error);
     });
+
